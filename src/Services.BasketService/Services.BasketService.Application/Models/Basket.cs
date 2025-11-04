480ff6fbdf7657723a057949a32919b33ca1de5b
trvannhanh <trvannhanh@gmail.com>
2025-11-04 14:09:43 +0700

    feat: BasketService (Clean Architecture, Redis Hash, TTL, Minimal API CRUD, Docker)

---
+++ src/Services.BasketService/Services.BasketService.Application/Models/Basket.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Services.BasketService.Application.Models
+{
+    public class Basket
+    {
+        public string UserId { get; set; } = default!;
+        public List<BasketItem> Items { get; set; } = new();
+    }
+}
---
+++ src/Services.BasketService/Services.BasketService.Application/Models/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Services.BasketService.Application.Models
+{
+    public class BasketItem
+    {
+        public string ProductId { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; } = default!;
+    }
+}
